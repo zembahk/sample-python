de453735628424f15a031ed2a297bc2af268303c
zembahk <77575523+zembahk@users.noreply.github.com>
2021-08-28 22:37:52 -0700

    1

--- server.py
+++ server.py
@@ -9,7 +9,10 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
+        if self.path == '/QR':
+            msg = '<img src="/img/QRhoge.png"></img>'
+        else:
+            msg = 'Hello! you requested %s' % (self.path)
         self.wfile.write(msg.encode())
 
 
